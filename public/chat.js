158b0dc18c6ebdbf2e9fb10ccc359dcc6800f2d4
BLAZE <blaze@MacBookPro.lan>
2026-06-08 21:22:03 +0530

    Fix duplicate socket

--- public/chat.js
+++ public/chat.js
@@ -1,27 +1,19 @@
-const socket=io();
-const user=localStorage.getItem('user')||'Guest';
-socket.on('loadMessages',m=>m.forEach(addMessage));
-socket.on('newMessage',addMessage);
-function send(){
-socket.emit('sendMessage',{sender:user,text:message.value});
-message.value='';
-}
-function addMessage(msg){
-const li=document.createElement('li');
-li.innerHTML='<b>'+msg.sender+'</b>: '+msg.text;
-messages.appendChild(li);
-}
 const socket = io();
-
+const user = localStorage.getItem("user") || "Guest";
+const message = document.getElementById("msg");
+const messages = document.getElementById("messages");
+socket.on('loadMessages', m => m.forEach(addMessage));
+socket.on('newMessage', addMessage);
 function sendMessage() {
-    const msg = document.getElementById("msg").value;
-
-    if(msg.trim() === "") return;
-
-    socket.emit("sendMessage", {
-        user: "Guest",
-        text: msg
+    socket.emit('sendMessage', {
+        sender: user,
+        text: message.value
     });
 
-    document.getElementById("msg").value = "";
+    message.value = "";
+}
+function addMessage(msg) {
+    const li = document.createElement('li');
+    li.innerHTML = '<b>' + msg.sender + '</b>: ' + msg.text;
+    messages.appendChild(li);
 }
